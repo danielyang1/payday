34b42516cad197a5cd0b086efe4f3b4540372f79
danielyang1 <dan0yang@outlook.com>
2016-01-04 10:40:19 -0600

    first

---
+++ weeklyPay.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var wage;
+var time;
+var total;
+
+wage = 22.50;
+time = 37;
+total = time * wage;
+
+console.log(wage + " +");
+console.log(time + " =");
+console.log(total + " dollars earned for the week");
