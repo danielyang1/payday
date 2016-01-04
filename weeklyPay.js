95cdb1e6fd7b86983e60dc90d10cd112e5d44662
danielyang1 <dan0yang@outlook.com>
2016-01-04 17:01:29 -0600

    function

--- weeklyPay.js
+++ weeklyPay.js
@@ -1,28 +1,31 @@
-// "use strict";
+"use strict";
+function money() {
+	var wage;
+	var time;
+	var regular;
+	var ot;
+	var otPay;
+	var round;
+	var otAmt;
 
-var wage;
-var time;
-var regular;
-var ot;
-var otPay;
-var round;
+	wage = prompt("How much per hour?");
+	time = prompt("Total hours worked?");
+	ot = time - 40;
+	regular = wage * time;
+	otAmt = ot * .5 * wage;
+	otPay = otAmt + regular;
+	round = Math.ceil(otPay * 100)/100;
 
-wage = prompt("How much per hour?");
-time = prompt("Total hours worked?");
-ot = time - 40;
-regular = wage * time;
-otAmt = ot * .5 * wage;
-otPay = otAmt + regular;
-round = Math.ceil(otPay * 100)/100;
-
-if (time > 40){
-	console.log(round);
+	if (time > 40){
+		console.log(round);
 	} else if (ot === 0){
 		console.log(regular);
 	} else {
 		console.log(regular);
+	}
 };
 
+money();
 
 /*  !! First Part of the Assignment !!
 var wage;
